6a5017f0d6126ba74950e0aeb84514c18c2923bf
Kafka <a50541853@gmail.com>
2018-12-26 20:09:53 +0800

    upload

---
+++ src/work/Login.java
@@ -0,0 +1,13 @@
+package work;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    JLabel userLabel = new JLabel("用 户 名");
+    JLabel severLabel = new JLabel("用 户 名");
+    JLabel userLabel = new JLabel("用 户 名");
+    JLabel userLabel = new JLabel("用 户 名");
+    JLabel userLabel = new JLabel("用 户 名");
+
+
+}
