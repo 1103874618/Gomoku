88887a82a7c8f44f0420e60c79dc2994855663f0
kafka <a50541853@gmail.com>
2018-12-31 16:07:09 +0800

    第二界面雏形

--- src/work/Hall.java
+++ src/work/Hall.java
@@ -1,5 +1,122 @@
 package work;
 
-public class Hall {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Flow;
 
+public class Hall extends JFrame {
+  JTabbedPane top = new  JTabbedPane(JTabbedPane.TOP);
+
+  JLabel topBarInside = new JLabel();//顶部便签容器
+  JPanel topBar = new JPanel();
+  JPanel body = new JPanel();
+
+  JPanel info = new JPanel();//左侧信息栏
+  JTabbedPane userInfo = new JTabbedPane();//
+  JTabbedPane severInfo = new JTabbedPane();
+
+  JPanel sit = new JPanel();//大厅主界面
+
+
+  JPanel userInfoF = new JPanel();
+  JPanel severInfoF = new JPanel();
+  JSplitPane hor = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,info,sit);
+  JSplitPane infoSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT,userInfoF,severInfoF);
+
+  public Hall(){
+    super("大厅界面");
+    //topBar.setLayout(null);
+    //top.addTab("游戏大厅",topBarInside);
+    hor.setOrientation(SwingConstants.VERTICAL);
+    hor.setDividerSize(6);
+
+    sit.setLayout(new BorderLayout());//大厅主界面
+    JPanel sitGroud = new JPanel();
+
+    JPanel topButton = new JPanel();
+    JLabel title = new JLabel("<<<  五子棋对战  >>>",JLabel.LEFT);
+    JButton exit = new JButton("退 出");
+    exit.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        dispose();
+      }
+    });
+    JButton sitDown = new JButton("自 动 入 座");
+
+//    exit.setPreferredSize(new Dimension(40,20));
+////    sitDown.setPreferredSize(new Dimension(40,20));
+////    exit.setSize(50,10);
+
+    topButton.setPreferredSize(new Dimension(650,30));
+    topButton.setLayout(new BorderLayout());
+    topButton.add(title,"West");
+    JPanel topButtonEast = new JPanel();//顶右部按钮
+    topButtonEast.setLayout(new BoxLayout(topButtonEast,BoxLayout.X_AXIS));
+    topButtonEast.setPreferredSize(new Dimension(250,10));
+    //topButtonEast.setBackground(Color.red);
+    topButtonEast.add(Box.createRigidArea(new Dimension(10, 0)));
+    topButtonEast.add(sitDown);
+    topButtonEast.add(Box.createRigidArea(new Dimension(10, 0)));
+    topButtonEast.add(exit);
+    topButtonEast.add(Box.createRigidArea(new Dimension(10, 0)));
+    topButton.add(topButtonEast,"East");
+
+    //用户信息栏
+    JPanel userInfoP = new JPanel();
+    JPanel severInfoP= new JPanel();
+    userInfoF.add(userInfo);
+    userInfoF.setLayout(new GridLayout(1,1));
+    severInfoF.setLayout(new GridLayout(1,1));
+    severInfoF.add(severInfo);
+    userInfo.addTab("                用户信息",userInfoP);
+    severInfo.addTab("               服务器信息",severInfoP);
+    info.setPreferredSize(new Dimension(250,670));
+    info.setBackground(Color.red);
+    info.setLayout(new BoxLayout(info,BoxLayout.X_AXIS));
+    info.add(infoSplit);
+    infoSplit.setDividerSize(6);
+    infoSplit.setDividerLocation(300);
+    userInfoF.setPreferredSize(new Dimension(250,335));
+    severInfoF.setPreferredSize(new Dimension(250,335));
+
+    sitGroud.setBackground(Color.blue);
+    sit.add(sitGroud,"Center");
+    sit.add(topButton,"North");
+
+
+
+    sit.setPreferredSize(new Dimension(650,670));
+    top.addTab("游戏大厅",body);
+    body.setSize(800,670);
+    body.add(hor);
+    top.setSize(800,670);
+
+
+    setLayout(null);
+    add(top);
+    //add(hor);
+    this.setSize(820,720);
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+  }
+
+  private  static  JPanel makePanel(String text){
+    JPanel p = new JPanel();
+    p.add(new Label(text));
+    p.setLayout(new GridLayout(1,1));
+    return p;
+  }
+
+
+}
+
+class HallDemo{
+  public static void main (String[] args){
+    Hall a= new Hall();
+  }
 }
--- src/work/Login.java
+++ src/work/Login.java
@@ -4,105 +4,133 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.http.WebSocket;
 
 public class Login extends JFrame {
-    JLabel userLabel = new JLabel("用 户 名");
-    JLabel serverLabel = new JLabel("服 务 器");
-    JLabel icon = new JLabel("头   像");
-    JLabel userInfo = new JLabel("请输入您的个人信息");
-    JLabel userImage = new JLabel(new ImageIcon("IconRes\\res\\face\\1-1.gif"), JLabel.LEFT);
-    JTextField userName = new JTextField();
-    JTextField serverIP = new JTextField();
+  JLabel userLabel = new JLabel("用 户 名");
+  JLabel serverLabel = new JLabel("服 务 器");
+  JLabel icon = new JLabel("头   像");
+  JLabel userInfo = new JLabel("请输入您的个人信息");
+  JLabel userImage = new JLabel(new ImageIcon("IconRes\\res\\face\\1-1.gif"), JLabel.LEFT);
+  JTextField userName = new JTextField();
+  JTextField serverIP = new JTextField();
 
-    public Login() {
-        super("登陆界面");
-        JPanel pNorth = new JPanel();
-        JPanel pSouth = new JPanel();
-        //北区
-        pNorth.setLayout(new BorderLayout());
-        pNorth.add(userInfo, "North");
-        //北区-东区
-        JPanel p11 = new JPanel();
-        p11.setLayout(new GridLayout(3, 1));
-        p11.add(userLabel);
-        p11.add(serverLabel);
-        p11.add(icon);
-        pNorth.add(p11, "West");
-        //北-中
-        JPanel p22 = new JPanel();
-        p22.setLayout(new GridLayout(3, 1));
-        p22.add(userName);
-        p22.add(serverIP);
-        p22.add(userImage);//
-        pNorth.add(p22, "Center");
+  public Login() {
+    super("登陆界面");
+    JPanel pNorth = new JPanel();
+    JPanel pSouth = new JPanel();
+    //北区
+    pNorth.setLayout(new BorderLayout());
+    pNorth.add(userInfo, "North");
+    //北区-东区
+    JPanel p11 = new JPanel();
+    p11.setLayout(new GridLayout(3, 1));
+    p11.add(userLabel);
+    p11.add(serverLabel);
+    p11.add(icon);
+    pNorth.add(p11, "West");
+    //北-中
+    JPanel p22 = new JPanel();
+    p22.setLayout(new GridLayout(3, 1));
+    p22.add(userName);
+    p22.add(serverIP);
+    p22.add(userImage);//
+    pNorth.add(p22, "Center");
 
-        //中区
-        JPanel faceGroud = new JPanel();
-        faceGroud.setLayout(null);
-        faceGroud.setSize(300, 300);
-        JButton buttons[] = new JButton[204];
+    //中区
+    JPanel faceGroud = new JPanel();
+    faceGroud.setLayout(null);
+    faceGroud.setSize(300, 300);
+    JButton buttons[] = new JButton[204];
 
-        String[] img = new String[65];
+    String[] img = new String[65];
 //
-        for (int i = 1; i <= 6; i++){
-            img[i-1] = "IconRes\\res\\face\\" + i + "-" + 1 + ".gif";
-        }
-        for (int i = 10; i <= 68; i++){
-            img[i-4] = "IconRes\\res\\face\\" + i + "-" + 1 + ".gif";
-        }
-
-
-        int j = 0;
-        for (int i = 0; i < 65; i++) {
-            JButton button = new JButton(new ImageIcon(img[i]));
-            int k = i;
-            button.setBounds(36 * (i % 10), (k / 10) * 36, 36, 36);//十个一行
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    Object b = e.getSource();
-                    JButton jb = (JButton) b;
-                    Icon i = jb.getIcon();
-                    userImage.setIcon(i);
-                }
-            });
-            faceGroud.add(button);
-        }
-
-        //南
-        JButton loginB = new JButton("连 接");
-        JButton resetB = new JButton("重 置");
-        JButton exitB = new JButton("退 出");
-        loginB.setBounds(0,0,60,36);
-        resetB.setBounds(0,0,60,36);
-        exitB.setBounds(0,0,60,36);
-        loginB.setSize(60,36);
-        resetB.setSize(60,36);
-        exitB.setSize(60,36);
-
-        pSouth.setLayout(null);
-
-        pSouth.add(loginB);
-        pSouth.add(resetB);
-        pSouth.add(exitB);
-
-        Container con = this.getContentPane();
-        con.setLayout(new BorderLayout());
-        con.add(pNorth, "North");
-        con.add(faceGroud, "Center");
-        con.add(pSouth, "South");
-
-
-        this.setVisible(true);
-        this.setSize(400, 450);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    for (int i = 1; i <= 6; i++) {
+      img[i - 1] = "IconRes\\res\\face\\" + i + "-" + 1 + ".gif";
     }
+    for (int i = 10; i <= 68; i++) {
+      img[i - 4] = "IconRes\\res\\face\\" + i + "-" + 1 + ".gif";
+    }
+
+
+    int j = 0;
+    for (int i = 0; i < 65; i++) {
+      JButton button = new JButton(new ImageIcon(img[i]));
+      int k = i;
+      button.setBounds(36 * (i % 10), (k / 10) * 36, 36, 36);//十个一行
+      button.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          Object b = e.getSource();
+          JButton jb = (JButton) b;
+          Icon i = jb.getIcon();
+          userImage.setIcon(i);
+        }
+      });
+      faceGroud.add(button);
+    }
+
+    //南
+    JButton loginB = new JButton("连 接");
+    JButton resetB = new JButton("重 置");
+    JButton exitB = new JButton("退 出");
+
+
+    //按钮事件
+    loginB.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        String[] userInfo = new String[2];
+        Icon userIco;
+        userInfo[0] = userName.getText();
+        userInfo[1] = serverIP.getText();
+        userIco = userImage.getIcon();
+        System.out.println("the user name is :" + userInfo[0]);
+        System.out.println("the IP is :" + userInfo[1]);
+      }
+    });
+    resetB.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        userName.setText("");
+        serverIP.setText("");
+        userImage.setIcon(new ImageIcon("IconRes\\res\\face\\1-1.gif"));
+      }
+    });
+
+    exitB.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        dispose();
+      }
+    });
+
+
+    pSouth.setLayout(new BoxLayout(pSouth, BoxLayout.X_AXIS));
+
+
+    pSouth.add(Box.createHorizontalGlue());
+    pSouth.add(loginB);
+    pSouth.add(Box.createRigidArea(new Dimension(20, 50)));
+    pSouth.add(resetB);
+    pSouth.add(Box.createRigidArea(new Dimension(20, 50)));
+    pSouth.add(exitB);
+    pSouth.add(Box.createHorizontalGlue());
+
+
+    Container con = this.getContentPane();
+    con.setLayout(new BorderLayout());
+    con.add(pNorth, "North");
+    con.add(faceGroud, "Center");
+    con.add(pSouth, "South");
+
+
+    this.setVisible(true);
+    this.setSize(400, 450);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 
 }
 
 class Demo {
-    public static void main(String[] args) {
-        Login l = new Login();
-    }
+  public static void main(String[] args) {
+    Login l = new Login();
+  }
 }
