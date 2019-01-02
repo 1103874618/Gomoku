8be0687a00bb38bbc162218bbbd981b5fe7c1ca8
kafka <a50541853@gmail.com>
2019-01-02 21:15:05 +0800

    简化注释版

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -21,7 +21,7 @@ public class ChatServer {
 
   public void start() {
     try {
-      ss = new ServerSocket(8888);//8888是tcp的端口号
+      ss = new ServerSocket(8888);
       statred = true;//连接状态
     } catch (BindException e1) {
       System.out.println("端口使用中...");
@@ -34,20 +34,18 @@ public class ChatServer {
       while (statred) {
         // boolean bConnected = false;
         Socket s = ss.accept();
-        Client c = new Client(s);//这里原本会报错,因为我们没办法在一个
-        // 静态的main方法里面new一个动态内部类,解决方法就是代码都放到
-        // 一个start方法里
+        Client c = new Client(s);
 
         System.out.println("a client connected");
         new Thread(c).start();//每起来一个链接我就new一个线程
         clients.add(c);//保存该线程类
         //dis.close();
       }
-    } catch (IOException e) {//说明读写socket过程中出错
+    } catch (IOException e) {
       e.printStackTrace();
     } finally {
       try {
-        ss.close();//整个serversocket关掉
+        ss.close();
       } catch (IOException e1) {
         e1.printStackTrace();
       }
@@ -87,7 +85,7 @@ public class ChatServer {
       //dis = new DataInputStream(s.getInputStream());//从连接中读取输入的字符流
       try {
         while (bConnected) {
-          // String str = dis.readUTF(); readutf是阻塞式的,客户端关闭以后会报错,并且第二个客户端无法连接
+          // String str = dis.readUTF();
           String str = dis.readUTF();
           //Object info = obi.readObject();
           //userInfo.add(info);
@@ -95,35 +93,24 @@ public class ChatServer {
 
           for (int i = 0; i < clients.size(); i++) {
             Client c = clients.get(i);
-            c.send(str);//服务器由此实现转发功能,若有一个客户端对象被关掉,
-            // 服务器再次调用其send方法时就会报错
+            c.send(str);//服务器由此实现转发功能
           }
 
-//          for (Iterator<Client> it = clients.iterator();it.hasNext();){
-//            Client c =it.next();
-//            c.send(str);//另一种实现
-//          }
 
-//          Iterator<Client> it = clients.iterator();
-//          while (it.hasNext()){
-//            Client c = it.next();
-//            c.send(str);//这样写也可以
-//          }
-          //这两个方法都需要进行锁定,但实际上没必要锁定,否则效率低
         }
       } catch (SocketException e3) {
-        clients.remove(this);//这里用this就是自己在调用send方法时出错了
+        clients.remove(this);//
         System.out.println("a client quit");
 
       } catch (EOFException e2) {
         System.out.println("a client closed");
-      } catch (IOException e) {//说明读写socket过程中出错
+      } catch (IOException e) {
         System.out.println("a client closed");
       } finally {
         try {
           if (dis != null) dis.close();//不等于null的话相当于未初始化,没必要关
           if (dos != null) dos.close();
-          if (s != null) s.close();//捕获到任何异常都要把s关掉
+          if (s != null) s.close();//
           clients.remove(this);
 
         } catch (IOException e1) {
